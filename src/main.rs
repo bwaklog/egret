da1452ef920c87c95be995d5f8dc1c5e86d53874
Aditya Hegde <burrawta@gmail.com>
2026-02-07 10:44:13 +0530

    chore: rename package to egret

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use crate::client::MatrixClient;
 use tracing::info;
 use tracing_subscriber::{EnvFilter, FmtSubscriber, fmt::time};
 
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("rot=info,matrix_sdk=error"));
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("egret=info,matrix_sdk=error"));
 
     let subscriber = FmtSubscriber::builder()
         .with_env_filter(filter)
